59e92beafc73d4590cce622021beaea35e3d4a24
angjaay <angga.angzai@gmai.com>
2021-07-21 11:28:55 +0700

    add restoman logo and icon

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../../assets/images/restoman-icon.png" type="image/x-icon">
     <title>Login Restoman</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/litera/bootstrap.min.css" />
