25837c7aee093d5fcb4f5776ce5ea4d1a6e1a850
ReiNewbbs <hirukorei@gmail.com>
2021-07-22 14:57:29 +0700

    Create PesananController

---
+++ function/SSL.php
@@ -0,0 +1,36 @@
+<?php
+
+class SSL
+{
+
+    public $word;
+    public $ciphering = "AES-128-CTR";
+    public $option = 0;
+    // Non-NULL Initialization Vector for encryption
+    public $encryption_iv = '1234567891011121';
+    private $encryption_key = "restoman";
+
+    /**
+     * Encrypting
+     * 
+     * @return string $encrypted
+     */
+    public function encr()
+    {
+        $encrypted = openssl_encrypt($this->word, $this->ciphering, $this->encryption_key, $this->option, $this->encryption_iv);
+
+        return $encrypted;
+    }
+
+    /**
+     * Decrypting
+     * 
+     * @return string $decrypted
+     */
+    public function decr()
+    {
+        $decrypted = openssl_decrypt($this->word, $this->ciphering, $this->encryption_key, $this->option, $this->encryption_iv);
+
+        return $decrypted;
+    }
+}
