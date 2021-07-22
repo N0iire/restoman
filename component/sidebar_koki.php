b67f0434f13d28f6481e58abb6339a1a7f0f8a63
angjaay <angga.angzai@gmai.com>
2021-07-22 14:58:47 +0700

    add sidebar to page

---
+++ component/sidebar_koki.php
@@ -0,0 +1,24 @@
+<div class="navigation">
+    <ul>
+        <li class="list active">
+            <b></b>
+            <b></b>
+            <a href=#>
+                <span class="icon">
+                    <ion-icon name="cash-outline"></ion-icon>
+                </span>
+                <span class="title">Pembayaran</span>
+            </a>
+        </li>
+        <li class="list">
+            <b></b>
+            <b></b>
+            <a href=#>
+                <span class="icon">
+                    <ion-icon name="receipt-outline"></ion-icon>
+                </span>
+                <span class="title">Laporan</span>
+            </a>
+        </li>
+    </ul>
+</div>
--- page/Pelayan-page/index.php
+++ page/Pelayan-page/index.php
@@ -1,8 +1,47 @@
 <!-- header -->
 <?php include '../../header.php' ?>
 
-<body>
+<body style="background: #fff;">
+    <div class="row">
+        <div class="col-sm-12">
+            <!-- navbar -->
+            <header>
+                <?php include '../../component/navbar.php' ?>
 
+            </header>
+        </div>
+
+        <div class="row" style="margin-top: 90px;">
+            <div class="col-sm-3">
+                <!-- sidebar -->
+                <aside>
+                    <?php include '../../component/sidebar_pelayan.php' ?>
+                </aside>
+            </div>
+
+            <div class="col-sm-9">
+                <!-- start page content -->
+                <?php
+                if (!isset($_GET['p'])) {
+                    require_once 'pemesanan.php';
+                } else if ($_GET['p'] == 'dashboard') {
+                    require_once $_GET['p'] . '.php';
+                } else if ($_GET['p'] == 'laporan' || $_REQUEST['p'] == 'laporan') {
+                    require_once  $_GET['p'] . '.php';
+                }
+                ?>
+
+            </div>
+
+        </div>
+
+    </div>
+    <div class="preloader">
+        <div class="box">
+
+        </div>
+
+    </div>
 
     <!-- end body -->
     <?php include '../../footer.php' ?>
