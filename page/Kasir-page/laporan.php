c0324f7d957c2adea4a864c1b34f0ec7c9d69df6
IkhsanNurulRizki <ikhsan.n.rizki@gmail.com>
2021-07-23 16:37:34 +0700

    Penambahan koki-page dan kasir-page

--- page/Kasir-page/laporan.php
+++ page/Kasir-page/laporan.php
@@ -1,1 +1,51 @@
-ini laporan
+<div class="col-sm-12" style="margin-top:10px; margin-left: -10px;">
+    <!-- start page content -->
+    <div class="card shadow-md ">
+        <div class="card-header text-white blue-head">
+            <h3>Data Laporan</h3>
+        </div>
+
+        <div class="card-body">
+            <table id="example" class="table table-borderless">
+                <thead>
+                    <tr>
+                        <th>ID Pesanan</th>
+                        <th>Atas Nama </th>
+                        <th>ID Meja</th>
+                        <th>Total Transaksi</th>
+                        <th style="width: 130px;">Lihat Detail Pesanan</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>test</td>
+                        <td>2812</td>
+                        <td>2</td>
+                        <td>
+                            <a href="#">
+                                <button class="btn btn-dark btn-block">
+                                    Detail
+                                </button>
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>test</td>
+                        <td>1922</td>
+                        <td>4</td>
+                        <td>
+                            <a href="#">
+                                <button class="btn btn-dark btn-block">
+                                    Detail
+                                </button>
+                            </a>
+                        </td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
