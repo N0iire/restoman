62c87219bc675bc648ab7a4fce8eb975f53edf13
angjaay <angga.angzai@gmai.com>
2021-07-23 08:52:37 +0700

    major update to pemesanan add pemesaanan layout

--- page/Pelayan-page/pemesanan.php
+++ page/Pelayan-page/pemesanan.php
@@ -1,1 +1,120 @@
-test
+<div class="col-lg-8">
+    <div class="row">
+        <div class="col-md-9">
+            <div class="row pt-1">
+
+                <div class="col-md-4 mb-1">
+                    <div class="container ">
+                        <div class="harga">26.000</div>
+                        <div class="menu">Ayam Bakar</div>
+                        <img src="../../assets/images/ayam bakar.jpeg" alt="ayam bakar" class="image shadow-dreamy">
+                        <div class="overlay">
+                            <button class="btn-grad">Tambah</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="container ">
+                        <div class="harga">26.000</div>
+                        <div class="menu">Ayam Bakar</div>
+                        <img src="../../assets/images/ayam bakar.jpeg" alt="ayam bakar" class="image shadow-dreamy">
+                        <div class="overlay">
+                            <button class="btn-grad">Tambah</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="container ">
+                        <div class="harga">26.000</div>
+                        <div class="menu">Ayam Bakar</div>
+                        <img src="../../assets/images/ayam bakar.jpeg" alt="ayam bakar" class="image shadow-dreamy">
+                        <div class="overlay">
+                            <button class="btn-grad">Tambah</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="container ">
+                        <div class="harga">26.000</div>
+                        <div class="menu">Ayam Bakar</div>
+                        <img src="../../assets/images/ayam bakar.jpeg" alt="ayam bakar" class="image shadow-dreamy">
+                        <div class="overlay">
+                            <button class="btn-grad">Tambah</button>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
+                <div class="col-md-3" style="position: fixed; margin-left:50%;">
+                    <div class="card cart" style="border-radius: 15px;">
+                        <div class="card-header cart blue-head text-white p-1" style="height: 90px;;">
+                            <h4>Pesan Baru</h4>
+                            <small>0 pesanan dalam keranjang </small>
+                        </div>
+                        <div class="card-body">
+
+
+                            <div class="row">
+                                <div class="col-md-1" style="padding-top:5px; margin-right:5px;">
+                                    <a href=""><i class="bi bi-trash align-self-center" style="font-size:1.5rem;  color:red !important;"></i></a>
+                                </div>
+                                <div class="col-md-6">
+                                    <span><b>ayam bakar</b></span><br>
+                                    <small>Rp. 26.000</small>
+                                </div>
+                                <div class="col-md-4">
+                                    <input class="form-control float-right" style="width: 60px; text-align: center;" value="1" type="number" name="jumlah" min="1" max="1">
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="card-footer">
+                            <div class="row mb-1">
+                                <div class="col-md-12 pull-left">
+                                    <input class="form-control" type="text" name="pembeli" placeholder="Atasnama Pembeli">
+                                </div>
+
+                            </div>
+                            <div class="row mb-1">
+                                <div class="col-md-4 float-left">
+                                    <small>Total <br>Pelanggan</small>
+                                </div>
+                                <div class="col-sm-3" style="margin-right: 10px;">
+                                    <input class="form-control" style="width: 70px; height:37px;" type="text" name="pembeli">
+                                </div>
+
+
+                                <div class=" col-sm-2 pull-left">
+                                    <select class="form-select" style=" width: 87px; padding-right: 7px;" aria-label="Default select example">
+                                        <option selected>Meja</option>
+                                        <option value="owner">1</option>
+                                        <option value="pelayan">2</option>
+                                        <option value="koki">3</option>
+                                        <option value="kasir">4</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="row mb-1">
+                                <div class="col-md-6 float-left">
+                                    <h4>Total :</h4>
+                                </div>
+                                <div class=" col-md-6 text-right">
+                                    Rp. 0
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <input type="submit" value="Bayar Pesanan" name="bayar" onclick="inputcart();" class="btn btn-danger btn-block">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
