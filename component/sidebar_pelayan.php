11b0eef9092ac5c5261d212f8251523287ba3218
angjaay <angga.angzai@gmai.com>
2021-07-23 10:31:38 +0700

    add functon get to sidebar pelayan

--- component/sidebar_pelayan.php
+++ component/sidebar_pelayan.php
@@ -1,9 +1,21 @@
 <div class="navigation">
     <ul>
+        <?php
+        if (!isset($_GET['p'])) {
+            echo '  <li class="list">
+            <b></b>
+            <b></b>
+            <a href="index.php">
+                <span class="icon">
+                    <ion-icon name="fast-food-outline"></ion-icon>
+                </span>
+                <span class="title">Semua</span>
+            </a>
+        </li>
         <li class="list">
             <b></b>
             <b></b>
-            <a href="">
+            <a href="?p=makanan">
                 <span class="icon">
                     <ion-icon name="fast-food-outline"></ion-icon>
                 </span>
@@ -13,13 +25,30 @@
         <li class="list">
             <b></b>
             <b></b>
-            <a href="">
+            <a href="?p=minuman">
                 <span class="icon">
                     <ion-icon name="fast-food-outline"></ion-icon>
                 </span>
                 <span class="title">Minuman</span>
             </a>
         </li>
+ ';
+        } else if ($_GET['p'] == 'meja' || $_REQUEST['p'] == 'meja') {
+            echo '
+            <li class="list active">
+            <b></b>
+            <b></b>
+            <a href="index.php">
+                <span class="icon">
+                    <ion-icon name="fast-food-outline"></ion-icon>
+                </span>
+                <span class="title">Pengelolaan Pesanan</span>
+            </a>
+        </li>
+        ';
+        }
+        ?>
+
 
         <!-- isset sidebar pengelolaan pesanan -->
 
