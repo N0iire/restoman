c04ea19fd116fe97db0f223e7a2664ebec83be56
primarazaq <primarazaq34@gmail.com>
2021-07-25 16:07:07 +0700

    fix bug

--- component/sidebar_pelayan.php
+++ component/sidebar_pelayan.php
@@ -1,5 +1,6 @@
 <div class="navigation">
     <ul>
+        <!-- isset sidebar pengelolaan pesanan -->
         <?php
         if (!isset($_GET['p'])) {
             echo '  <li class="list">
@@ -33,9 +34,10 @@
             </a>
         </li>
  ';
+            // isset pengelolaan meja
         } else if ($_GET['p'] == 'meja' || $_REQUEST['p'] == 'meja') {
             echo '
-            <li class="list active">
+            <li class="list">
             <b></b>
             <b></b>
             <a href="index.php">
@@ -50,7 +52,7 @@
         ?>
 
 
-        <!-- isset sidebar pengelolaan pesanan -->
+
 
 
 
