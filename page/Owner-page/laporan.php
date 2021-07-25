967580a75a8fa0f2bfefe13a9771d7a4cf056340
primarazaq <primarazaq34@gmail.com>
2021-07-25 16:08:01 +0700

    alert test onclick button

--- page/Owner-page/laporan.php
+++ page/Owner-page/laporan.php
@@ -26,7 +26,7 @@
                         <td>2021/04/25</td>
                         <td>
                             <a href="#">
-                                <button class="btn btn-dark btn-block">
+                                <button class="btn btn-dark btn-block" data-toggle="modal" data-target="#exampleModalCenter">
                                     Detail
                                 </button>
                             </a>
@@ -40,7 +40,7 @@
                         <td>2021/07/25</td>
                         <td>
                             <a href="#">
-                                <button class="btn btn-dark btn-block">
+                                <button class="btn btn-dark btn-block" data-toggle="modal" data-target="#exampleModalCenter">
                                     Detail
                                 </button>
                             </a>
@@ -49,6 +49,67 @@
 
                 </tbody>
             </table>
+            <br><br>
+            <div class="row">
+                <div class="col">
+                    <a href="#">
+                        <button class="btn btn-outline-secondary btn-sm">
+                            Cetak Laporan
+                        </button>
+                    </a>
+                </div>
+                <div class="col">
+                    <p class="text-right mt-auto" for="pendapatan">Total Pendapatan Per-Bulan </p>
+                </div>
+                <div class="col">
+                    <input type="text" class="form-control ml-auto" id="pendapatan" name="pendapatan" style="width: 200px; height: 30px;" readonly>
+                </div>
+            </div>
         </div>
     </div>
+</div>
+
+
+
+<!-- Modal POPUP -->
+<div class="container">
+
+    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+
+            <div class="modal-content">
+
+                <div class="modal-header text-white blue-head">
+                    <h4 class="modal-title" id="exampleModalLongTitle">Detail Laporan : ../../..</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col ml-2">
+                            <p>Nama Menu</p>
+                            <p>Nama Menu</p>
+                        </div>
+                        <div class="col">
+                            <p>Jumlah</p>
+                            <p>Jumlah</p>
+                        </div>
+                    </div><br><br>
+                    <div class="row">
+                        <div class="col ml-2">
+                            <h5>Total Harga</h5>
+                        </div>
+                        <div class="col">
+                            <h5>Rp.999.999</h5>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 </div>
