89bf8553dbda8b047c5e5dfb61b5f3e84ee07cab
primarazaq <primarazaq34@gmail.com>
2021-08-07 08:22:49 +0700

    show side bar by kategori

--- component/sidebar_pelayan.php
+++ component/sidebar_pelayan.php
@@ -3,37 +3,33 @@
         <!-- isset sidebar pengelolaan pesanan -->
         <?php
         if (!isset($_GET['p'])) {
-            echo '  <li class="list">
-            <b></b>
-            <b></b>
-            <a href="index.php">
-                <span class="icon">
-                    <ion-icon name="fast-food-outline"></ion-icon>
-                </span>
-                <span class="title">Semua</span>
-            </a>
-        </li>
-        <li class="list">
-            <b></b>
-            <b></b>
-            <a href="?p=makanan">
-                <span class="icon">
-                    <ion-icon name="fast-food-outline"></ion-icon>
-                </span>
-                <span class="title">Makanan</span>
-            </a>
-        </li>
-        <li class="list">
-            <b></b>
-            <b></b>
-            <a href="?p=minuman">
-                <span class="icon">
-                    <ion-icon name="fast-food-outline"></ion-icon>
-                </span>
-                <span class="title">Minuman</span>
-            </a>
-        </li>
- ';
+        ?>
+            <li class="list">
+                <b></b>
+                <b></b>
+                <a href="index.php">
+                    <span class="icon">
+                        <ion-icon name="fast-food-outline"></ion-icon>
+                    </span>
+                    <span class="title">Semua</span>
+                </a>
+            </li>
+            <?php
+            foreach ($kategori->get_all() as $data) {
+            ?>
+                <li class="list">
+                    <b></b>
+                    <b></b>
+                    <a href="?k=<?php echo $data['nama_kategori']; ?>&i=<?php echo $data['id_kategori']; ?>">
+                        <span class="icon">
+                            <ion-icon name="fast-food-outline"></ion-icon>
+                        </span>
+                        <span class="title"><?php echo $data['nama_kategori']; ?></span>
+                    </a>
+                </li>
+        <?php
+            }
+
             // isset pengelolaan meja
         } else if ($_GET['p'] == 'meja' || $_REQUEST['p'] == 'meja') {
             echo '
@@ -46,8 +42,8 @@
                 </span>
                 <span class="title">Pengelolaan Pesanan</span>
             </a>
-        </li>
-        ';
+            </li>
+            ';
         }
         ?>
 
