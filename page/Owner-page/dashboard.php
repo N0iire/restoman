c02f4dd4cb8f84da3e9c91deef4318384d04d785
primarazaq <primarazaq34@gmail.com>
2021-08-08 13:07:07 +0700

    show all data to card dashboard

--- page/Owner-page/dashboard.php
+++ page/Owner-page/dashboard.php
@@ -18,9 +18,9 @@
     }
 </style>
 
+
 <div class="container-fluid">
     <section id="minimal-statistics">
-
         <div class="row">
             <div class="col-12 mt-2 mb-1">
                 <h4 class="text-uppercase">
@@ -31,7 +31,7 @@
         </div>
         <div class="row">
 
-
+            <?php $data_pendapatan = $pembayaran->get_sum(); ?>
             <!-- large card -->
             <div class="row">
                 <div class="col-xl-6 col-md-12">
@@ -40,10 +40,10 @@
                             <div class="card-body cleartfix">
                                 <div class="media align-items-stretch">
                                     <div class="align-self-center">
-                                        <h1 class="mr-2" style="color: #18ab6b;">Rp.7.456.000</h1>
+                                        <h1 class="mr-2" style="color: #18ab6b;">Rp.<?php echo $data_pendapatan['pendapatan'] ?></h1>
                                     </div>
-                                    <div class="media-body">
-                                        <h4>Total Pendapatan</h4>
+                                    <div class="media-body text-right mr-1">
+                                        <h4>Total <br>Pendapatan</h4>
 
                                     </div>
                                     <div class="align-self-center">
@@ -61,9 +61,9 @@
                             <div class="card-body cleartfix">
                                 <div class="media align-items-stretch">
                                     <div class="align-self-center">
-                                        <h1 class="mr-2" style="color: #1abd91;">$999999</h1>
+                                        <h1 class="mr-2" style="color: #1abd91;"><?php echo $data_pendapatan['jml_transaksi'] ?></h1>
                                     </div>
-                                    <div class=" media-body">
+                                    <div class=" media-body text-right mr-2">
                                         <h4>Jumlah <br> Transaksi</h4>
                                     </div>
                                     <div class="align-self-center">
@@ -76,6 +76,7 @@
                 </div>
             </div>
 
+            <?php $data_pengunjung = $pesanan->get_sum(); ?>
             <!-- small card -->
 
             <div class="col-xl-3 col-sm-6 col-12">
@@ -84,8 +85,8 @@
                         <div class="card-body">
                             <div class="media d-flex">
                                 <div class="media-body text-left">
-                                    <h3 class="primary">278</h3>
-                                    <span>Jumlah Pengunjung</span>
+                                    <h3 class="primary"><?php echo $data_pengunjung['jml_pelanggan'] ?></h3>
+                                    <span>Jumlah <br> Pengunjung</span>
                                 </div>
                                 <div class="align-self-center">
                                     <ion-icon name="people-outline" style="color: #1ebdbd; font-size: 50px; margin-left: 10px;"></ion-icon>
@@ -99,6 +100,7 @@
                 </div>
             </div>
 
+            <?php $data_pegawai = $user->get_count(); ?>
             <div class="col-xl-3 col-sm-6 col-12">
                 <div class="card">
                     <div class="card-content">
@@ -106,7 +108,7 @@
                             <div class="media d-flex">
                                 <div class="media-body text-left">
                                     <h3 class="warning">
-                                        1000
+                                        <?php echo $data_pegawai['jmlpegawai'] ?>
                                     </h3>
                                     <span>Jumlah <br>Pegawai</span>
                                 </div>
@@ -122,13 +124,14 @@
                 </div>
             </div>
 
+            <?php $data_menu = $menu->get_count(); ?>
             <div class="col-xl-3 col-sm-6 col-12">
                 <div class="card">
                     <div class="card-content">
                         <div class="card-body">
                             <div class="media d-flex">
                                 <div class="media-body text-left">
-                                    <h3 class="success">64</h3>
+                                    <h3 class="success"><?php echo $data_menu['jmlmenu'] ?></h3>
                                     <span>Jumlah <br> Menu</span>
                                 </div>
                                 <div class="align-self-center">
@@ -143,14 +146,15 @@
                 </div>
             </div>
 
+            <?php $data_menu2 = $menu->get_count2(); ?>
             <div class="col-xl-3 col-sm-6 col-12">
                 <div class="card">
                     <div class="card-content">
                         <div class="card-body">
                             <div class="media d-flex">
                                 <div class="media-body text-left">
-                                    <h3 class="danger">423</h3>
-                                    <span>Konfirmasi Menu</span>
+                                    <h3 class="danger"><?php echo $data_menu2['jmlmenu'] ?></h3>
+                                    <span>Konfirmasi <br> Menu</span>
                                 </div>
                                 <div class="align-self-center">
                                     <ion-icon name="checkmark-done-outline" style="color: #a12755; font-size: 50px; margin-left: 10px;"></ion-icon>
